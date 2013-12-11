5c7efd635be3349aa3e8300131d8c0cb0b1b884a
Eric S. Raymond <esr@thyrsus.com>
2013-12-11 12:18:26 -0500

    More speed optimization.  Regression tests pass.

--- export.c
+++ export.c
@@ -206,7 +206,7 @@ static void export_commit(rev_commit *commit, char *branch, int strip)
     rev_file	*f, *f2;
     int		i, j, i2, j2;
     struct fileop *operations, *op, *op2;
-    int noperations;
+    int noperations, cmp;
 
     if (reposurgeon)
     {
@@ -231,7 +231,10 @@ static void export_commit(rev_commit *commit, char *branch, int strip)
 		    rev_dir	*dir2 = commit->parent->dirs[i2];
 		    for (j2 = 0; j2 < dir2->nfiles; j2++) {
 			f2 = dir2->files[j2];
-			if (strcmp(f->name, f2->name) == 0) {
+			cmp = strcmp(f->name, f2->name);
+			if (cmp > 0) /* file lists are sorted by name */
+			    break;
+			if (cmp == 0) {
 			    present = true;
 			    changed = (f->serial != f2->serial);
 			    break;
@@ -296,7 +299,10 @@ static void export_commit(rev_commit *commit, char *branch, int strip)
 		    rev_dir	*dir2 = commit->dirs[i2];
 		    for (j2 = 0; j2 < dir2->nfiles; j2++) {
 			f2 = dir2->files[j2];
-			if (strcmp(f->name, f2->name) == 0) {
+			cmp = strcmp(f->name, f2->name);
+			if (cmp > 0) /* file lists are sorted by name */
+			    break;
+			if (cmp == 0) {
 			    present = true;
 			    break;
 			}
