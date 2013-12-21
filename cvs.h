86a7f6556728ab8d1e4788b2f631a77ed256359e
Eric S. Raymond <esr@thyrsus.com>
2013-12-21 11:39:51 -0500

    Cut the maximum brabchiness to 10 to squeeze the working set.

--- cvs.h
+++ cvs.h
@@ -56,7 +56,7 @@
 #endif
 
 /* CVS_MAX_BRANCHWIDTH should match the number in the longrev test*/
-#define CVS_MAX_BRANCHWIDTH	12
+#define CVS_MAX_BRANCHWIDTH	10
 #define CVS_MAX_DEPTH		(2*CVS_MAX_BRANCHWIDTH + 2)
 #define CVS_MAX_REV_LEN	(CVS_MAX_DEPTH * 11)
 
--- tests/longrev.tst
+++ tests/longrev.tst
@@ -12,7 +12,7 @@ co.write("README", "A test of multiple tags.\n")
 co.add("README")
 co.commit("Initial revision")
 
-for i in range(12):
+for i in range(10):
     branchname = ("branch%s" % (i+1))
     co.branch( branchname )
     co.switch( branchname )
